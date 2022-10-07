effff1c532074e73b3cd877c4e25b3f80320e208
saurabhdaware <saurabhdaware99@gmail.com>
2022-10-07 21:45:52 +0530

    fix: esm requires without extension

--- plugin/index.js
+++ plugin/index.js
@@ -1,5 +1,5 @@
-import { vitePluginMdToHTML } from "./vite-plugin";
-export { mdToHTML } from "./md-to-html";
+import { vitePluginMdToHTML } from "./vite-plugin.js";
+export { mdToHTML } from "./md-to-html.js";
 
 export { vitePluginMdToHTML };
 export default vitePluginMdToHTML;
